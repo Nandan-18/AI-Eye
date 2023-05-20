8db18d4de8626a10217d1e75446f18b267bf4e8a
Craig Lobo <craiglobo1@gmail.com>
2023-05-20 12:40:00 -0600

    added game class

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
 #main function
+import pygame as pg
+import sys
+
+class Game:
+    def __init__(self) -> None:
+        pg.init()
+        self.win = pg.display.set_mode(size=(500,500))
+        self.clock = pg.time.Clock()
+
+        self.fps = 60
+
+        pg.display.set_caption("Game")
+
+
+    def load(self):
+        pass
+
+    def update(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+                
+        
+        pg.display.update()
+        self.clock.tick(self.fps)
+
+    def run(self):
+        self.load()
+        while True:
+            self.update()
+    
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()

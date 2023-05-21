c025e3a844806469f1b435c0b15a9e7d7d0e3d44
wezragraham <wezragraham@gmail.com>
2023-05-21 14:16:28 -0600

    Revert "Merge branch 'david' into william"
    This reverts commit 7094647f46ffb924b43fdd0872ddb23046ccdac1, reversing
    changes made to 224e5a9bafdff050f08d7195563d0d29d2c00d4b.

--- clients/stable_diffusion.py
+++ clients/stable_diffusion.py
@@ -1,94 +1,36 @@
 import os
-import requests
-import httpx
-import io
-from io import BytesIO
-import pygame as pygame
-
 from dotenv import load_dotenv
 load_dotenv()
 REPLICATE_API_KEY = os.getenv('REPLICATE_API_KEY')
 
 # replicate must be imported after load_dotenv
 import replicate
+from replicate import Client
 
-# Example usage
-# stable_diffusion_client = StableDiffusionClient()
-# stable_diffusion_client.run("A cat holding a gameboy console")
 class StableDiffusionClient:
+    # Example usage
+    # client = StableDiffusionClient()
+    # stable_diffusion_client.run("A cat holding a gameboy console")
+
     def __init__(
         self,
         model: str = "stability-ai/stable-diffusion",
         version: str = "27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478",
-        image_dimensions: list = [512, 512],
     ):
         self.model = model
         self.version = version
         self.api_token = REPLICATE_API_KEY
-        self.image_dimensions = image_dimensions
 
-    def load_image(
-        self,
-        urls: list,
-    ) -> BytesIO:
-        """
-        Loads image synchronously
-
-        Example:
-            image_bytes = load_image(urls)
-            image = pygame.image.load(image_bytes)
-        """
-        response = requests.get(urls[0])
-        image_bytes = io.BytesIO(response.content)
-        return image_bytes
-
-    async def aload_image(
-        self,
-        urls: list,
-    ) -> BytesIO:
-        """
-        Loads image asynchronously
-
-        Example:
-            image_bytes = load_image(urls)
-            image = pygame.image.load(image_bytes)
-        """
-        response = httpx.get(urls[0])
-        image_bytes = io.BytesIO(response.content)
-        return image_bytes
-
-    def run(
+    async def run(
         self, 
         prompt: str,
-    ) -> BytesIO:
-        """
-        Runs Stable Diffusion synchronously
-        """
+    ):
         replicate_client = replicate.Client(api_token=self.api_token)
         replicate_model = f"{self.model}:{self.version}"
-        urls = replicate_client.run(
+        output = replicate_client.run(
             replicate_model,
-            input={"prompt": prompt, "image_dimensions": self.image_dimensions},
+            input={"prompt": prompt},
         )
-        image_bytes = self.load_image(urls)
-        return image_bytes
-
-    async def arun(
-        self, 
-        prompt: str,
-    ) -> BytesIO:
-        """
-        Runs Stable Diffusion asynchronously
-        """
-        replicate_client = replicate.Client(api_token=self.api_token)
-        replicate_model = f"{self.model}:{self.version}"
-        urls = replicate_client.run(
-            replicate_model,
-            input={"prompt": prompt, "image_dimensions": self.image_dimensions},
-        )
-        image_bytes = await self.aload_image(urls)
-        return image_bytes
-
-stable_diffusion_client = StableDiffusionClient()
+        return output
 
 
--- main.py
+++ main.py
@@ -3,15 +3,12 @@ import pygame as pg
 import sys
 import logging
 from scripts import ui
-from clients.stable_diffusion import stable_diffusion_client
-from clients import utils
-import asyncio
 
 class Game:
     def __init__(self) -> None:
         pg.mixer.pre_init(16000, -16, 2, 2048)
         pg.init()
-        self.win = pg.display.set_mode(size=(1000,1000))
+        self.win = pg.display.set_mode(size=(1000,500))
         self.clock = pg.time.Clock()
 
         self.fps = 60
@@ -26,9 +23,6 @@ class Game:
 
 
     def load(self):
-        if  self.playing == False:
-            self.text_input = ui.TextInput((100,100), "AI Game Jam Game")
-            self.button = ui.Button((275,200), (400, 50),"Start")
         
             self.mute_button = ui.Button((800, 10), (100, 50),"Mute")
 
@@ -36,17 +30,15 @@ class Game:
                 self.text_input = ui.TextInput((100,100), "AI Game Jam Game")
                 self.button = ui.Button((275,200), (400, 50),"Start")
 
-            pg.mixer.music.load('sounds/JeopardyTypeBeat.mp3')
-            pg.mixer.music.play(-1)
-            
-        if self.playing == True:
-            self.text_input = ui.TextInput((100,100), "pizza")
-            self.button = ui.Button((10,10), (100, 50),"hey")
-            self.image = pg.surface.Surface((512,512))
-            self.word = utils.FileUtils.get_random_word()
+                pg.mixer.music.load('sounds/JeopardyTypeBeat.mp3')
+                pg.mixer.music.play(-1)
+                
+            if self.playing == True:
+                self.text_input = ui.TextInput((100,100), "pizza")
+                self.button = ui.Button((10,10), (100, 50),"hey")
 
-            pg.mixer.music.load('sounds/Suspense.mp3')
-            pg.mixer.music.play(-1)
+                pg.mixer.music.load('sounds/Suspense.mp3')
+                pg.mixer.music.play(-1)
 
 
     def update(self):
@@ -54,50 +46,12 @@ class Game:
         self.clock.tick(self.fps)
         mouse_pos = pg.mouse.get_pos()
         mouse_buttons = pg.mouse.get_pressed()
-
         events = pg.event.get()
         for event in events:
+
             if event.type == pg.QUIT:
                 self.quit()
-            if self.playing == True:
-                if event.type == pg.KEYDOWN:
-                    # If you press RIGHT arrow key, run synchronously
-                    if event.key == pg.K_RIGHT:
-                        image_bytes = stable_diffusion_client.run(
-                            prompt="A dog holding a gameboy console",
-                        )
-                        self.image = pg.image.load(
-                            image_bytes, "assets/placeholder.svg"
-                        )
-                    # If you press LEFT arrow key, run asynchronously
-                    elif event.key == pg.K_LEFT:
-                        # Change this to change the style of the art
-                        preprompt = "Isometic art of "
-
-                        prompt = preprompt + self.word
-
-                        # Load placeholder while the image is generating
-                        self.image = pg.image.load(
-                            "assets/placeholder.svg"
-                        )
-
-                        loop = asyncio.get_event_loop()
-                        coroutine = stable_diffusion_client.arun(
-                            prompt=prompt
-                        )
-                        logging.info("Generating image...")
-                        image_bytes = loop.run_until_complete(coroutine)
-                        logging.info("Image generated!")
-
-                        # Pygame needs a name for the image file even if it's 
-                        # not going to be saved, so we just use a placeholder.
-                        self.image = pg.image.load(
-                            image_bytes, "assets/placeholder.svg"
-                        )
-
-                        # Get a new word for the next round
-                        self.word = utils.FileUtils.get_random_word()
-
+                
         self.text_input.update(events)
         self.button.update(mouse_buttons, mouse_pos)
         self.mute_button.update(mouse_buttons, mouse_pos)
@@ -124,13 +78,6 @@ class Game:
         self.text_input.draw(self.win)
         self.button.draw(self.win)
         self.mute_button.draw(self.win)
-
-        if self.playing == True:
-            self.win.blit(pg.transform.scale(
-                self.image, 
-                tuple(stable_diffusion_client.image_dimensions)), 
-                (0,200),
-            )
 
     def draw_start_screen(self):
         self.win.fill((250,248,246))

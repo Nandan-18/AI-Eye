b5428c10b79e86b809f351e1112e6caa21ef8a45
taikon <davidma100@gmail.com>
2023-05-21 16:30:40 -0600

    Fix issue where StableDiffusionClient causes Pygame to think game has frozen.

--- main.py
+++ main.py
@@ -43,14 +43,15 @@ class Game:
             pg.mixer.music.play(-1)
             
         if self.playing == True:
-            self.text_input = ui.TextInput((100,100), "pizza")
+            self.text_input = ui.TextInput((100,100), "", True)
             self.button = ui.Button((10,10), (100, 50),"hey")
             self.image = pg.surface.Surface((512,512))
-            self.word = utils.FileUtils.get_random_word()
 
             pg.mixer.music.load('sounds/Suspense.mp3')
             pg.mixer.music.play(-1)
 
+        self.word = utils.FileUtils.get_random_word()
+        self.has_generated_image = False
         self.dialogue_sys = dialouge.DialougeSystem()
 
 
@@ -65,52 +66,30 @@ class Game:
             if event.type == pg.VIDEORESIZE:
                 self.win = pg.display.set_mode((event.w, event.h), pg.RESIZABLE)
 
-            
             if event.type == pg.QUIT:
                 self.quit()
+
             if self.playing == True:
-                if event.type == pg.KEYDOWN:
-                    # If you press RIGHT arrow key, run synchronously
-                    if event.key == pg.K_RIGHT:
-                        image_bytes = stable_diffusion_client.run(
-                            prompt="A dog holding a gameboy console",
-                        )
-                        self.image = pg.image.load(
-                            image_bytes, "assets/placeholder.svg"
-                        )
-                    # If you press LEFT arrow key, run asynchronously
-                    elif event.key == pg.K_LEFT:
-                        # Change this to change the style of the art
-                        preprompt = "Isometic art of "
+                preprompt = "Isometic art of "
+                prompt = preprompt + self.word
 
-                        prompt = preprompt + self.word
+                logging.info("Generating image...")
+                image_bytes = stable_diffusion_client.run_new(
+                    prompt=prompt,
+                )
 
-                        # Load placeholder while the image is generating
-                        self.image = pg.image.load(
-                            "assets/placeholder.svg"
-                        )
-
-                        loop = asyncio.get_event_loop()
-                        coroutine = stable_diffusion_client.arun(
-                            prompt=prompt
-                        )
-                        logging.info("Generating image...")
-                        image_bytes = loop.run_until_complete(coroutine)
-                        logging.info("Image generated!")
-
-                        # Pygame needs a name for the image file even if it's 
-                        # not going to be saved, so we just use a placeholder.
-                        self.image = pg.image.load(
-                            image_bytes, "assets/placeholder.svg"
-                        )
-
-                        # Get a new word for the next round
-                        self.word = utils.FileUtils.get_random_word()
+                # Pygame needs a name for the image file even if it's 
+                # not going to be saved, so we just use a placeholder.
+                self.image = pg.image.load(
+                    image_bytes, "assets/placeholder.svg"
+                )
+                logging.info("Image generated!")
 
         self.text_input.update(events)
         self.button.update(mouse_buttons, mouse_pos)
         self.dialogue_sys.update(events)
         self.mute_button.update(mouse_buttons, mouse_pos)
+
 
         if self.text_input.shake == 30:
             self.wrong_answer.play()
@@ -131,12 +110,6 @@ class Game:
         self.text_input.draw(self.win)
         self.dialogue_sys.draw(self.win)
 
-
-    def draw_start_screen(self):
-        self.win.fill((250,248,246))
-        self.button.draw(self.win)
-        self.mute_button.draw(self.win)
-
         if self.playing == True:
             self.win.blit(pg.transform.scale(
                 self.image, 
@@ -144,10 +117,12 @@ class Game:
                 (0,200),
             )
 
+
     def draw_start_screen(self):
         self.win.fill((250,248,246))
         self.button.draw(self.win)
         self.mute_button.draw(self.win)
+
 
     def run(self):
         self.load()

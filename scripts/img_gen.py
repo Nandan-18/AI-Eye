bd2fb2df8b684afdb2c1c07fdb20c56bb673a1fb
taikon <davidma100@gmail.com>
2023-05-22 15:37:59 -0600

    Add image border.

--- scripts/img_gen.py
+++ scripts/img_gen.py
@@ -4,6 +4,9 @@ import pygame as pg
 import threading
 from loguru import logger
 
+# delete this
+import time
+
 class ImageGenerator:
     def __init__(self, image_dimensions: list = [512,512]) -> None:
         self.client = StableDiffusionClient(image_dimensions=image_dimensions)
@@ -35,6 +38,11 @@ class ImageGenerator:
                 logger.debug("Correct!")
                 # self.image = pg.image.load("assets/correct_placeholder.jpeg")
 
+                # When the user gets the word right, we want to reset has_generated_image to False
+                # so that the next word can be generated. So uncomment the line below when implemented.
+                # self.has_generated_image = False
+
+
     def generate_image(self, prompt):
         image_bytes = self.client.run(
             prompt=prompt,
@@ -53,4 +61,31 @@ class ImageGenerator:
             win.get_height()/3 - self.image.get_height()//2,
         )
         win.blit(pg.transform.scale(self.image,self.image_dimensions), pos)
+
+        # Create a border around the image if image has been generated.
+        if self.has_generated_image == True:
+            # Isla, change the two variables below to change the border around the image
+            border_color = (0,0,0)
+            border_thickness = 50
+
+            pg.draw.rect(
+                win, border_color, (*pos, *self.image_dimensions), border_thickness
+            )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

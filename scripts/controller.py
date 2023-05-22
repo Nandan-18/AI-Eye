4e922c4baada5fc257810b0c00f712db64220daf
Craig Lobo <craiglobo1@gmail.com>
2023-05-22 10:50:40 -0600

    something

---
+++ scripts/controller.py
@@ -0,0 +1,14 @@
+import pygame as pg
+from .animator import Animator
+from .dialogue import DialogueSystem
+from .scoring import Score
+
+
+class gameController:
+    def __init__(self) -> None:
+        pass
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
